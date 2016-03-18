bcbc11848eac5b895797a60e04fd1b7d98492ee1
Gaston Traberg <samelat@gmail.com>
2016-03-18 16:15:20 -0300

    developing commands execution

--- tunnels/http/remote.php
+++ tunnels/http/remote.php
@@ -2,24 +2,40 @@
 
 class Stream {
 
+    public $sock = null;
+
+    public function __construct() {
+        $this->sock = 
+    }
 }
 
 class Tunnel {
-    public $running = False;
-    public $messages = [];
     public $streams = array();
 
     public function __construct() {
-        $this->running = True;
+        
     }
 
     public function handler() {
+
         $running = True;
+        $incoming = [];
+        $outgoing = [];
+
         while ($running) {
-            sleep(8);
+
             @session_start();
+            $incoming = array_merge($incoming, $_SESSION['incoming']);
+            $_SESSION['outgoing'] = array_merge($_SESSION['outgoing'], $outgoing);
             $running = $_SESSION['running'];
             @session_commit();
+
+            foreach ($incoming as &$message) {
+                switch($message['cmd']) {
+                    case 'connect':
+                        $stream = new Stream($message['host'], $message['port'])
+                }
+            }
         }
     }
 }
@@ -31,15 +47,17 @@ if ($method == 'POST') {
 
     $request = json_decode(file_get_contents('php://input'), true);
     $seq = $request['seq'];
-    $message = $request['msg'];
+    $messages = $request['msgs'];
+    $command = $request['cmd'];
 
-    switch($message['cmd']) {
+    switch($command) {
         case 'start':
             if (!isset($_SESSION['running'])) {
                 $_SESSION['running'] = True;
                 $_SESSION['seq'] = 0;
                 $_SESSION['buffer'] = array();
                 $_SESSION['outgoing'] = [];
+                $_SESSION['incoming'] = [];
                 @session_commit();
 
                 $tunnel = new Tunnel();
@@ -49,15 +67,18 @@ if ($method == 'POST') {
 
         case 'sync':
             if ($seq == $_SESSION['seq']) {
-                array_push($_SESSION['outgoing'], $message);
+                $_SESSION['outgoing'] = array_merge($_SESSION['outgoing'], $messages);
                 $_SESSION['seq']++;
+
                 while (array_key_exists($_SESSION['seq'], $_SESSION['buffer'])) {
                     array_push($_SESSION['outgoing'], $_SESSION['buffer'][$_SESSION['seq']]);
+                    $_SESSION['outgoing'] = array_merge($_SESSION['outgoing'], $_SESSION['buffer'][$_SESSION['seq']]);
                     unset($_SESSION['buffer'][$_SESSION['seq']]);
                     $_SESSION['seq']++;
                 }
+
             } else {
-                $_SESSION['buffer'][$seq] = $message;
+                $_SESSION['buffer'][$seq] = $messages;
             }
             break;
 
